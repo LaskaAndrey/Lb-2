58f0f3d22bdccb6a350459891759942bf327fbb3
unknown <kylymnyk.n@gmail.com>
2020-11-28 22:31:36 +0200

    third commit

--- public/tableOfUsers.php
+++ public/tableOfUsers.php
@@ -1,30 +1,9 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-<link rel="stylesheet" href="assets\css\table.css">
-
-<!doctype html>
-<html lang="en">
-<head>
-  
-<div class="header">
-			<div class="container">
-				<a href="index.php" class="logo">
-          <img src="assets\img\logo.png" width="220" height="220"
-            alt="" style="position:absolute;top:0px;left:50px"/>
-          </a>
-        </div>
-   </div>
-
-<h1><a  href="login_page.php" style="position:absolute;top:70px;right:250px">SIGN IN</a></h1>
-<h1><a  href="registration.php" style="position:absolute;top:70px;right:100px">SIGN UP</a></h1>
-</head>
-
-<?php session_start(); ?>
 <table class="table" >
     <thead>
         <th>#</th>
         <th>First Name</th> 
         <th>Last Name</th>
-        <th>Email</th>
+        <th>Login</th>
         <th>Role</th>
     </thead>
     <tbody>
@@ -45,7 +24,7 @@
                         echo "<td>".$rowUser['id']."</td>";
                         echo "<td>".$rowUser['first_name']."</td>";
                         echo "<td>".$rowUser['last_name']."</td>";
-                        echo "<td>".$rowUser['login']."</td>";
+                        echo "<td>".$rowUser['email']."</td>";
 
                         $queryRole = "SELECT title FROM roles WHERE id = '{$rowUser['role_id']}'";
                         $resultRole = mysqli_query($conn, $queryRole);
@@ -57,6 +36,3 @@
         ?>
     </tbody> 
 </table>
-</body>
-
-</html>
